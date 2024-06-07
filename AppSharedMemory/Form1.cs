9d4b61b4d89a9424436bbe3b107d4a71317910bd
Mouhamad Gueye <seydinag023@gmail.com>
2024-06-07 23:26:50 +0000

    Ajoutez des fichiers projet.

---
+++ AppSharedMemory/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppSharedMemory
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            service = new ServiceMetier.Service1Client();
+        }
+
+        ServiceMetier.Service1Client service;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
